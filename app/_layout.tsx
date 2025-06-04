8490cd243836b68075f642193ffadcdaa5796619
Nicholas Glasgow <pcpro2009@gmail.com>
2025-06-04 00:22:57 -0400

    Updated _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,7 @@ import { StatusBar } from 'expo-status-bar';
 import { SplashScreen } from 'expo-router';
 import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 import { ThemeProvider } from '@/contexts/ThemeContext';
-import { AuthProvider } from '@/contexts/AuthContext';
+import { AuthProvider, useAuth } from '@/contexts/AuthContext';
 import { useColorScheme } from 'react-native';
 import { useFonts } from 'expo-font';
 
