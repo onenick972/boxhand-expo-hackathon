6ebbe5e41c91330073caa5e6af3152845e54e2c9
Nicholas Glasgow <pcpro2009@gmail.com>
2025-06-03 23:29:22 -0400

    Updated sign-in.tsx

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -7,7 +7,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { ArrowLeft } from 'lucide-react-native';
 
 export default function SignInScreen() {
-  const { theme } = u  seTheme();
+  const { theme } = useTheme();
   const { signIn, isLoading } = useAuth();
   const router = useRouter();
 
